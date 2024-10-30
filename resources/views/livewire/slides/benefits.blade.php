30c46c865529dd158c530e90da1d3098a479a7d1
Samuel Levy <sam@determineddevelopment.com>
2024-10-30 18:59:25 +1000

    Ok, good enough NOW now

--- resources/views/livewire/slides/benefits.blade.php
+++ resources/views/livewire/slides/benefits.blade.php
@@ -3,7 +3,6 @@
     <p class="text-lg">A solution looking for a problem</p>
     <h3 class="text-bold text-5xl">You don't need a lot of database compute</h3>
     <p>You can use a tiny RDS instance because connections aren't held open for long.</p>
-  </div>
   <div x-transition x-show="$wire.slide >= 1" wire:ignore class="flex flex-col gap-10">
     <h3 class="text-bold text-5xl">Multi-database multi-tenancy with a single database</h3>
     <p>Multi-database multi-tenancy sucks. You can give each tenant their own DB without provisioning more infra.</p>
@@ -20,5 +19,6 @@
   </div>
   <div x-transition x-show="$wire.slide === 5" wire:ignore class="flex flex-col gap-10">
     <p class="text-xl">Lose your data.</p>
+  </div>
   </div>
 </x-slides.title-and-body>
